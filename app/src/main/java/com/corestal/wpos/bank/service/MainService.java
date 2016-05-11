9b5f1ea3f68fdf884d9d6e3bf2da0e9b4fcba74f
cgz <vsjosonadmin@163.com>
2016-05-11 13:23:48 +0800

    MainServiceImpl

--- app/src/main/java/com/corestal/wpos/bank/service/MainService.java
+++ app/src/main/java/com/corestal/wpos/bank/service/MainService.java
@@ -2,6 +2,7 @@ package com.corestal.wpos.bank.service;
 
 import com.corestal.wpos.bank.biz.constant.BizConstants;
 import com.corestal.wpos.bank.biz.entity.FunctionMenu;
+import com.corestal.wpos.bank.biz.entity.Order;
 import com.corestal.wpos.bank.common.CSApplicationHolder;
 
 import java.util.ArrayList;
@@ -12,37 +13,19 @@ import java.util.List;
  * MainActivityService
  * 主要服务
  */
-public class MainService {
-    public void initApplication(){
-        // TODO 这里初始化应该是从属性文件中取出数据
-        // TODO 此处为了测试直接赋值
-        CSApplicationHolder.setStartNo(0);
-        CSApplicationHolder.setStationCount(6);
+public interface MainService {
+    /**
+     * 初始化数据
+     */
+    public void initApplication();
 
-        List<FunctionMenu> functionMenuList = new ArrayList<FunctionMenu>();
-        FunctionMenu functionMenu;
+    /**
+     * 取号操作
+     * @return
+     */
+    public String takeNo(FunctionMenu functionMenu);
 
-        functionMenu = new FunctionMenu();
-        functionMenu.setCode("A");
-        functionMenu.setId(1);
-        functionMenu.setName("个人业务");
-        functionMenu.setStatus(BizConstants.FUNCTION_MENU_STATUS_USE);
-        functionMenuList.add(functionMenu);
+    public void print(Order order);
 
-        functionMenu = new FunctionMenu();
-        functionMenu.setCode("B");
-        functionMenu.setId(1);
-        functionMenu.setName("对公业务");
-        functionMenu.setStatus(BizConstants.FUNCTION_MENU_STATUS_USE);
-        functionMenuList.add(functionMenu);
-
-        functionMenu = new FunctionMenu();
-        functionMenu.setCode("C");
-        functionMenu.setId(1);
-        functionMenu.setName("综合业务");
-        functionMenu.setStatus(BizConstants.FUNCTION_MENU_STATUS_USE);
-        functionMenuList.add(functionMenu);
-
-        CSApplicationHolder.setFunctionMenuList(functionMenuList);
-    }
+    public void done(Order order);
 }
